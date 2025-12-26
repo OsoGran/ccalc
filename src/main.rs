bb71f58f49d029926313ad5c76eb3909c8379d76
Max Tacker <tacker.maxwell@gmail.com>
2025-12-26 15:45:07 -0600

    Implemented algorithm

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 struct Cli {
     /// The algorithm to look for
     algo: String,
-    /// The path to look for
+    /// The path to look for 
     path: std::path::PathBuf,
 }
 
@@ -12,8 +12,22 @@ fn main() {
     let args = Cli::parse();
 
     println!("algorithm: {:?}, path: {:?}", args.algo, args.path)
+
+    let data: Vec<u16> = vec![0xDEAD, 0xBEEF, 0xCAFE];
+    let data_slice: &[u16] = &data;
+
+    let output: u16 = checksum_1760(data_slice);
+
+    println!("Checksum: {:?}", output);
 }
 
-fn checksum_1760(data: &[u16]) -> u16 {
-    //
+fn checksum_1760(data: &[u16]) -> Result<&u16, &str> {
+    // Handle empty slice
+    data.first().ok_or("Slice was empty")
+
+    let mut checksum = 0;
+    for (index, word) in data.iter().enumerate(){
+        checksum = checksum ^ (word.rotate_right(index));
+    }
+    Ok(checksum);
 }
